641754a650f163302d5897f1aa0d4084abad4b84
dedy.kusworo <dedy.kusworo@gmail.com>
2024-11-13 06:04:50 +0700

    update other message in biller trxs

---
+++ database/migrations/2024_11_12_230128_update_trx_bill.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTrxBill extends Migration
+{
+    // php artisan migrate --path=/database/migrations/2024_11_12_230128_update_trx_bill.php
+    // php artisan migrate:rollback --path=/database/migrations/2024_11_12_230128_update_trx_bill.php
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('biller_trxs', function (Blueprint $table) {
+            $table->text('other_msg')->nullable()->change();
+            // $table->string('other_msg');
+        });
+    }
+}
